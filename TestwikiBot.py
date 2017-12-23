74802b849ef211d4380edfe50fe3e1ccc0161d8d
Kenny2github <kenny2minecraft@gmail.com>
2017-12-23 18:14:00 +0800

    Major updates!
    +Handle redirects better
    *CHANGE TO USE mwparserfromhell - MUCH MORE RELIABLE
    -Squashed small bugs

--- TestwikiBot.py
+++ TestwikiBot.py
@@ -1,12 +1,13 @@
 import re, difflib, pickle, time
 import easygui as e
 import mw_api_client as mw
+import mwparserfromhell as mwp
 
 with open('login.txt', 'r') as logininfo:
     en = mw.Wiki(logininfo.readline().strip())
     en_name = logininfo.readline().strip()
     en_pass = logininfo.readline().strip()
-    tw = mw.Wiki(logininfo.readline().strip())
+    tw = mw.Wiki(logininfo.readline().strip(), 'what is wrong with my user agent jeez')
     tw_name = logininfo.readline().strip()
     tw_pass = logininfo.readline().strip()
 
@@ -14,7 +15,7 @@ with open('login.txt', 'r') as logininfo:
 en_tok = en.post_request(action='login',
                          lgname=en_name)['login']['token']
 en.post_request(action='login', lgname=en_name,
-                lgpassword=en_pass, ltoken=en_tok)
+                lgpassword=en_pass, lgtoken=en_tok)
 tw.login(tw_name, tw_pass)
 
 en_ap = en.allpages(namespace=0, limit=300)
@@ -25,6 +26,8 @@ try:
 except IOError:
     seen = []
 
+en_ap = en.allpages(namespace=0, limit=300, apfrom=seen[-1])
+
 try:
     with open('config/index.pickle', 'rb') as f:
         index = pickle.load(f)
@@ -33,11 +36,13 @@ except IOError:
 
 def index_trans(cur, title, content):
     idx = {}
-    matches = re.findall('\|([a-zA-Z]+)=([^}|\n]+)', content)
+    content = mwp.parse(content)
     idx[title] = {}
-    for ns, name in matches:
-        idx[title][ns] = name
+    for param in content.filter_templates()[0].params:
+        idx[title][str(param.name)] = str(param.value)
     cur.update(idx)
+
+cont = False
 
 try:
     for en_p in en_ap:
@@ -45,33 +50,42 @@ try:
             print('\nAlready seen {}, skipping'.format(en_p.title))
             continue
         seen.append(en_p.title)
-        tw_p = tw.page('Eng:' + en_p.title)
+        tw_p = tw.page(('' if ':' in en_p.title else 'Eng:') + en_p.title)
         print('\nPage {}'.format(tw_p.title))
         en_content = en_p.read()
         try:
             tw_content = tw_p.read()
+        except mw.requests.exceptions.ConnectionError as e:
+            print(' Disconnected?', e)
+            continue
         except mw.NotFound:
             print(' Test Wiki page {} not found, creating...'.format(tw_p.title))
-            en_mod_content = re.sub(r'\[\[(?![^|\]]*:)([^\]#][^\]|]+)\]\]',
-                                        r'[[Eng:\1|\1]]',
-                                        en_content)
-            en_mod_content = re.sub(r'\[\[(?![^|\]]*:)([^\]#][^\]|]+)\|([^\]]+)\]\]',
-                                    r'[[Eng:\1|\2]]',
-                                    en_mod_content)
+            en_mod_content = mwp.parse(en_content, 0, True)
+            for link in en_mod_content.ifilter_wikilinks():
+                if link.title.startswith('#') or ':' in link.title:
+                    continue
+                if link.text is None:
+                    link.text = str(link.title)
+                link.title = 'Eng:' + str(link.title)
+            for template in en_mod_content.ifilter_templates():
+                if template.name == 'April Fools':
+                    en_mod_content.remove(template)
+            en_mod_content = str(en_mod_content).strip()
             if not re.search('^#REDIRECT', en_mod_content, re.I):
                 en_mod_content = '{{/translate}}' + en_mod_content
             print('Creation',
                   tw_p.edit(en_mod_content.strip(),
                             'Automated edit: Created Test Wiki copy \
 of English Wiki page.')['edit']['result'])
-            tran_cont = '{{translate\n' \
-                        + '|Eng={0}\n|En={0}'.format(en_p.title) \
-                        + '\n}}'
-            index_trans(index, tw_p.title, tran_cont)
-            print('Creation of Translate Page',
-                  tw.page(tw_p.title + '/translate').edit(tran_cont,
-                                                          'Automated edit: \
-Created translate page for {}.'.format(tw_p.title))['edit']['result'])
+            if not re.search('^#REDIRECT', en_mod_content, re.I):
+                tran_cont = '{{translate\n' \
+                            + '|Eng={0}\n|En={0}'.format(en_p.title) \
+                            + '\n}}'
+                index_trans(index, tw_p.title, tran_cont)
+                print('Creation of Translate Page',
+                      tw.page(tw_p.title + '/translate').edit(tran_cont,
+                                                              'Automated edit: \
+    Created translate page for {}.'.format(tw_p.title))['edit']['result'])
             continue
         if not re.search('^#REDIRECT', en_content.strip(), re.I):
             try:
@@ -87,40 +101,55 @@ Created translate page for {}.'.format(tw_p.title))['edit']['result'])
 Created translate page for {}.'.format(tw_p.title))['edit']['result'])
             print(' Index /translate page')
             index_trans(index, tw_p.title, tran_cont)
-        tw_mod_content = re.sub(r'\[\[Eng:([^|\]]+)\]\]', r'[[\1]]', tw_content)
-        tw_mod_content = re.sub(r'\[\[Eng:([^|\]]+)\|\1\]\]', r'[[\1]]', tw_mod_content)
-        tw_mod_content = re.sub(r'\[\[Eng:([^|\]]+)\|([^|\]]+)\]\]', r'[[\1|\2]]', tw_mod_content)
-        tw_mod_content = re.sub(r'{{[^}]*/translate}}\n?', '', tw_mod_content)
+        tw_mod_content = mwp.parse(tw_content, 0, True)
+        for link in tw_mod_content.ifilter_wikilinks():
+            if link.title.startswith('Eng:'):
+                if link.title[4:] == link.text:
+                    link.text = None
+                link.title = link.title[4:]
+        for template in tw_mod_content.ifilter_templates():
+            if template.name.endswith('/translate'):
+                tw_mod_content.remove(template)
         diff = '\n'.join(difflib.unified_diff(tw_mod_content.splitlines(), en_content.splitlines(), en_p.title, tw_p.title))
         if en_content.strip() != tw_mod_content.strip():
             resp = e.codebox('Confirm that an edit is needed:', 'Confirm edit on {}'.format(tw_p.title), diff)
             if resp is not None:
                 resp = resp.strip() or None
             if resp is not None:
-                en_mod_content = re.sub(r'\[\[(?![^|\]]*:)([^\]#][^\]|]+)\]\]',
-                                        r'[[Eng:\1|\1]]',
-                                        en_content)
-                en_mod_content = re.sub(r'\[\[(?![^|\]]*:)([^\]#][^\]|]+)\|([^\]]+)\]\]',
-                                        r'[[Eng:\1|\2]]',
-                                        en_mod_content)
-                m = re.search('{{[^}]*/translate}}\n?', tw_content)
-                if m is not None:
-                    en_mod_content = m.group(0) + en_mod_content
+                en_mod_content = mwp.parse(en_content, 0, True)
+                for link in en_mod_content.ifilter_wikilinks():
+                    if link.title.startswith('#') or ':' in link.title:
+                        continue
+                    if link.text is None:
+                        link.text = str(link.title)
+                    link.title = 'Eng:' + str(link.title)
+                tw_par_content = mwp.parse(tw_content, 0, True)
+                for template in tw_par_content.ifilter_templates():
+                    if template.name.endswith('/translate'):
+                        en_mod_content.insert(0, template)
+                    if template.name == 'April Fools':
+                        en_mod_content.remove(template)
                 print('Edit',
-                      tw_p.edit(en_mod_content.strip(),
+                      tw_p.edit(str(en_mod_content).strip(),
                                 'Automated edit: Updated Test Wiki copy \
 of English Wiki page.')['edit']['result'])
         else:
             print(' Identical content, skipping')
             continue
         time.sleep(5)
+except KeyboardInterrupt:
+    if raw_input('Continue on to next section?'):
+        cont = True
+    else:
+        raise
 finally:
     with open('config/seenap.pickle', 'wb') as f:
         pickle.dump(seen, f, -1)
     with open('config/index.pickle', 'wb') as f:
         pickle.dump(index, f, -1)
 
-raise SystemExit
+if not cont:
+    raise SystemExit
 
 en_rc = en.allpages(namespace='0', prefix='Eng:() lists block', limit=50)
 seen_titles = []
